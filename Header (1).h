11ca9e2f8542aafb15dc949424d12ea3a3525a4f
mimiji <btssuny13@yahoo.com>
2022-10-24 15:39:43 +0300

    В один файл

--- Header (1).h
+++ Header (1).h
@@ -1,4 +1,6 @@
-﻿#include <iostream>
+﻿#pragma once
+
+#include <iostream>
 #include <random>
 #include <sstream>
 
--- Source_3.cpp
+++ Source_3.cpp
@@ -1,65 +1,15 @@
-#include "Header.h"
+#include "Values.h"
 
-char *input(int size)
-{
-	char* values = new char[size];
+char* input(size_t size);
+string out_values(char* values, const size_t size);
+char find_max(char* values, const size_t size);
+void delete_values(char*& values);
 
-	const size_t min_range = 0;
-	const size_t max_range = 26;
-
-	random_device rd;
-	mt19937 gen(rd());
-	const uniform_int_distribution<> uniformIntDistribution(min_range, max_range);
-
-	for (const size_t i = 0; i < size; i++)
-	{
-		const size_t temp = uniformIntDistribution(gen);
-		values[i] = temp + 'a';
-	}
-
-	return values;
-}
-
-string out_values(char* values, int size)
-{
-	ostringstream tmpStream;
-	for (const size_t i = 0; i < size - 1; i++)
-	{
-		tmpStream << values[i] << "\t";
-	}
-	tmpStream << values[size - 1];
-
-	return tmpStream.str();
-}
-
-char find_max(char* values, int size)
-{
-	const size_t maximum = 0;
-
-	for (const size_t i = 0; i < size; i++)
-	{
-		if (values[i] > maximum)
-			maximum = values[i];
-	}
-
-	return maximum;
-}
-
-void delete_values(char* values)
-{
-	if (values != nullptr)
-	{
-		delete[] values;
-		values = nullptr;
-	}
-}
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	char* values;
 	int size = 0;
-
 	cout << "Укажите размер массива: ";
 	cin >> size;
 
@@ -70,7 +20,7 @@ int main()
 	}
 
 	// Создаем массив
-	values = input(size);
+	char* values = input(size);
 	// Выводим полученный массив
 	cout << "Полученный массив:\n" << out_values(values, size);
 	// Выводим максимальный элемент
@@ -79,4 +29,58 @@ int main()
 	delete_values(values);
 
 	return 0;
+}
+
+char* input(size_t size)
+{
+	char* values = new char[size];
+
+	const size_t min_range = 0;
+	const size_t max_range = 26;
+
+	random_device rd;
+	mt19937 gen(rd());
+	const uniform_int_distribution<> uniformIntDistribution(min_range, max_range);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		const size_t temp = uniformIntDistribution(gen);
+		values[i] = temp + 'a';
+	}
+
+	return values;
+}
+
+string out_values(char* values, const size_t size)
+{
+	ostringstream tmpStream;
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		tmpStream << values[i] << "\t";
+	}
+	tmpStream << values[size - 1];
+
+	return tmpStream.str();
+}
+
+char find_max(char* values, const size_t size)
+{
+	size_t maximum = 0;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		if (values[i] > maximum)
+			maximum = values[i];
+	}
+
+	return maximum;
+}
+
+void delete_values(char*& values)
+{
+	if (values != nullptr)
+	{
+		delete[] values;
+		values = nullptr;
+	}
 }
